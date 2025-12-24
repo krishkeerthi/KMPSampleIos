3d31ca7b10baf1dec1385007b23ced67cc7c75ed
keerthi-15363 <keerthi.kc@zohocorp.com>
2025-12-24 14:42:42 +0530

    Initial Commit

---
+++ SampleDB client/SampleDB_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDB_clientApp.swift
+//  SampleDB client
+//
+//  Created by Keerthi K on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleDB_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleDB clientTests/SampleDB_clientTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDB_clientTests.swift
+//  SampleDB clientTests
+//
+//  Created by Keerthi K on 24/12/25.
+//
+
+import Testing
+@testable import SampleDB_client
+
+struct SampleDB_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
